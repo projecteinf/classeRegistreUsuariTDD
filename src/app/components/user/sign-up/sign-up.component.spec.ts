f37ffdda009f56ff8c7e1150342477c2542f17fb
Projecte Informatica <projecteinf@boscdelacoma.cat>
2024-09-23 18:23:08 +0300

    Enable button if all data is correct

--- src/app/components/user/sign-up/sign-up.component.spec.ts
+++ src/app/components/user/sign-up/sign-up.component.spec.ts
@@ -10,6 +10,9 @@ function getNthLabel(index:number) {
   return compiled.querySelectorAll("label")[index];
 }
 
+function getInputFromLabel(labelIndex:number) {
+  return getNthLabel(labelIndex)!.querySelector("input");
+}
 
 describe('Layout', () => {
   
@@ -71,11 +74,6 @@ describe('Layout', () => {
     )
   });
 
-
-  function getInputFromLabel(labelIndex:number) {
-    return getNthLabel(labelIndex)!.querySelector("input");
-  }
-
 });
 
 describe('SignUpComponent - Button send', () => {
@@ -101,6 +99,28 @@ describe('SignUpComponent - Button send', () => {
   });
 
   it('Enable button if all data is correct', () => {
-    
+    const languages=["frances","angles","italia","catala","castella"].sort();
+    const inputUserName:HTMLInputElement = getInputFromLabel(0)!;
+    const inputEmail:HTMLInputElement = getInputFromLabel(1)!;
+    const inputPassword:HTMLInputElement = getInputFromLabel(2)!;
+    const inputRepeatPassword:HTMLInputElement = getInputFromLabel(3)!;
+    const selectLanguage:HTMLSelectElement = getNthLabel(3)!.querySelector("select")!;
+    const button:HTMLButtonElement = compiled.querySelector("button")!;
+
+    inputUserName.value="Maria";
+    inputEmail.value="maria@gmail.com";
+    inputPassword.value="Pa$$w0rd";
+    inputRepeatPassword.value="Pa$$w0rd";
+    selectLanguage.value=languages[2];
+
+    inputUserName.dispatchEvent(new Event("input"));
+    inputEmail.dispatchEvent(new Event("input"));
+    inputPassword.dispatchEvent(new Event("input"));
+    inputRepeatPassword.dispatchEvent(new Event("input"));
+    selectLanguage.dispatchEvent(new Event("change"));
+
+    fixture.detectChanges();
+    expect(button.disabled).toBeFalse();
+
   })
 });
