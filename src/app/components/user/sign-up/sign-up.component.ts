05dad69d5c711a783209f74c14021dc5f482fc76
Projecte Informatica <projecteinf@boscdelacoma.cat>
2024-09-23 18:53:08 +0300

    Implementing Enable button if all data is correct

--- src/app/components/user/sign-up/sign-up.component.ts
+++ src/app/components/user/sign-up/sign-up.component.ts
@@ -1,17 +1,27 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 export class SignUpComponent {
-  
+  username!:string;
+  email!:string;
+  language!:string;
+
   getLanguages():string[] {
     return ["frances","angles","italia","catala","castella"].sort();
   }
 
+  isDisabled():boolean {
+    return this.email==="" || this.email === undefined;
+  }
+  save() {
+    console.log(this.email,this.username,this.language);
+  }
 
 }
