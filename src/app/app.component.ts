05caee3078cbb3d54d0cb253a85cdf70151baad5
Projecte Informatica <projecteinf@boscdelacoma.cat>
2024-10-01 20:19:26 +0300

    Implementació link de menú

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterOutlet } from '@angular/router';
 import { SignUpComponent } from "./components/user/sign-up/sign-up.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, SignUpComponent],
+  imports: [RouterOutlet, RouterLink, SignUpComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { SignUpComponent } from './components/user/sign-up/sign-up.component';
+import { ListComponent } from './components/user/list/list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'signup', component: SignUpComponent},
+    {path: 'listusers', component: ListComponent}
+];
