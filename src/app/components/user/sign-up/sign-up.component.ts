6098cd1fa1b26a58aa4a6519cce59d407292c46a
isard <jrodr236@xtec.cat>
2024-10-03 15:51:12 +0200

    Afegits dos quadres de text d'Angular Material

--- src/app/components/user/sign-up/sign-up.component.ts
+++ src/app/components/user/sign-up/sign-up.component.ts
@@ -2,11 +2,18 @@ import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { User } from '../../../classes/user';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
 
 @Component({
   selector: 'app-sign-up',
   standalone: true,
-  imports: [FormsModule, MatSlideToggleModule],
+  imports: [
+    FormsModule, 
+    MatSlideToggleModule,
+    MatFormFieldModule, 
+    MatInputModule
+  ],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
