b28d18ced59c4e29f0cda0526dd56c1f7961a3dc
Projecte Informatica <projecteinf@boscdelacoma.cat>
2024-10-09 18:28:54 +0300

    API validació amb i sense errors

--- src/app/components/user/list/list.component.ts
+++ src/app/components/user/list/list.component.ts
@@ -12,14 +12,25 @@ import { Api } from '../../../services/api';
 })
 
 export class ListComponent implements OnInit {
+  errorMessage!:String;
+  private _isError:boolean=false;
 
   constructor(private api:Api ) {}
 
   ngOnInit() {
     const itemsp=25;
-      this.api.getCats(itemsp).subscribe( data => {
+    this.api.getCats(itemsp).subscribe( {
+      next: (data) => {
+        this.isError=false;
         console.log(data);
-      });
-  } 
-  
+      },
+      error: (err) => {
+        this.isError=true;
+        this.errorMessage="Error en la connexió amb l'API. Contacti amb l'administrador";
+      }
+    });
+  }
+
+  get isError() { return this._isError; }
+  set isError(error:boolean) { this._isError=error; }
 }
--- src/app/test/components/user/list.component.spec.ts
+++ src/app/test/components/user/list.component.spec.ts
@@ -2,11 +2,14 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ListComponent } from '../../../components/user/list/list.component';
 import { Api } from '../../../services/api';
 import { provideHttpClient } from '@angular/common/http';
-import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { HttpTestingController, provideHttpClientTesting } from "@angular/common/http/testing";
+import { of, throwError } from 'rxjs';
 
 describe('ListComponent', () => {
   let component: ListComponent;
   let fixture: ComponentFixture<ListComponent>;
+  let serviceApi: Api;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -22,9 +25,36 @@ describe('ListComponent', () => {
     fixture = TestBed.createComponent(ListComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    serviceApi = TestBed.inject(Api);
+    httpTestingController = TestBed.inject(HttpTestingController)
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('Cats retrieve OK', () => {
+    const mockData = [ {} ];
+    spyOn(serviceApi, 'getCats').and.returnValue(of(mockData));
+
+    component.ngOnInit();
+    fixture.detectChanges();
+    
+    const compiled:HTMLElement = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector(".error-message")).toBeFalsy();
+  });
+
+  it('Cats retrieve error', () => {
+    const errMessage = "Error en la connexió amb l'API. Contacti amb l'administrador";
+
+    spyOn(serviceApi, 'getCats').and.
+      returnValue(throwError(()=> Error("Error en la connexió amb l'API. Contacti amb l'administrador")));
+
+    component.ngOnInit();
+    fixture.detectChanges();
+
+    const compiled:HTMLElement = fixture.nativeElement as HTMLElement;
+    expect(compiled.querySelector(".error-message")?.textContent).toContain(errMessage);
+
   });
 });
